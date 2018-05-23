9698549ddd9eea14a4b3d704f8207a165c9d790f
MaximBogun <maximboguns@gmail.com>
2018-05-23 12:49:18 +0300

    init with 3 tasks

---
+++ src/com/company/AddTwoNumbers.java
@@ -0,0 +1,63 @@
+package com.company;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) { val = x; }
+ * }
+ */
+
+import java.math.BigInteger;
+
+public class AddTwoNumbers {
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    private ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        BigInteger numberOne = getNumberFromListNode(l1);
+        BigInteger numberTwo = getNumberFromListNode(l2);
+        StringBuilder sumNumbers = new StringBuilder();
+        sumNumbers.append(numberOne.add(numberTwo));
+        ListNode firstNode = new ListNode(0);
+        ListNode lastNode = firstNode;
+        char[] numericChar = sumNumbers.reverse().toString().toCharArray();
+        lastNode.val = Character.getNumericValue(numericChar[0]);
+        for (int i = 1; i < numericChar.length; i++) {
+            lastNode.next = new ListNode(Character.getNumericValue(numericChar[i]));
+            lastNode = lastNode.next;
+        }
+        return firstNode;
+    }
+
+
+    private BigInteger getNumberFromListNode(ListNode l) {
+        StringBuilder nodeNumber = new StringBuilder();
+        while (l != null) {
+            nodeNumber.append(l.val);
+            l = l.next;
+        }
+        return nodeNumber.length() == 0 ? new BigInteger("0")
+                : new BigInteger(nodeNumber.reverse().toString());
+    }
+
+    public static void main(String[] args) {
+        AddTwoNumbers addTwoNumbers = new AddTwoNumbers();
+        ListNode listNode = new ListNode(2);
+        listNode.next = new ListNode(4);
+        listNode.next.next = new ListNode(3);
+
+        ListNode listNode2 = new ListNode(5);
+        listNode2.next = new ListNode(6);
+        listNode2.next.next = new ListNode(4);
+        addTwoNumbers.addTwoNumbers(listNode, listNode2);
+    }
+}
---
+++ src/com/company/ReverseInteger.java
@@ -0,0 +1,43 @@
+package com.company;
+/*
+* Given a 32-bit signed integer, reverse digits of an integer.
+
+Example 1:
+
+Input: 123
+Output: 321
+Example 2:
+
+Input: -123
+Output: -321
+Example 3:
+
+Input: 120
+Output: 21
+Note:
+Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range:
+[−231,  231 − 1]. For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.*/
+
+import java.math.BigInteger;
+
+public class ReverseInteger {
+    public int reverse(int x) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(x);
+        stringBuilder.reverse();
+        if ((x >= 0 && new BigInteger(stringBuilder.toString())
+                .compareTo(new BigInteger(String.valueOf(Integer.MAX_VALUE))) > 0)
+                || x < 0 && new BigInteger("-" + stringBuilder.toString().replace("-", ""))
+                .compareTo(new BigInteger(String.valueOf(Integer.MIN_VALUE))) < 0) {
+            return 0;
+        }
+        return x > 0 ? Integer.valueOf(stringBuilder.toString()) :
+                -Integer.valueOf(stringBuilder.toString().replace("-", ""));
+    }
+
+    public static void main(String[] args) {
+        ReverseInteger reverseInteger = new ReverseInteger();
+        reverseInteger.reverse(123);
+        reverseInteger.reverse(-123);
+    }
+}
